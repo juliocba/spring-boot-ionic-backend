f42243480a469766f5eb9e82a6bc8ec14524cd04
Julio Baltazar <julio.baltazar@ifactory.com.br>
2018-09-06 11:47:18 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/jcba/cursomc/domain/Endereco.java
+++ src/main/java/com/jcba/cursomc/domain/Endereco.java
@@ -39,7 +39,6 @@ public class Endereco implements Serializable {
 
 	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
 			Cliente cliente, Cidade cidade) {
-		super();
 		this.id = id;
 		this.logradouro = logradouro;
 		this.numero = numero;
