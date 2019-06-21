4cc5e1cb411e0c7d0994b5e80b69574162df38d1
julio baltazar <juliocesar.jcba@gmail.com>
2019-06-21 11:37:03 -0300

    email html

--- src/main/java/com/jcba/cursomc/services/EmailService.java
+++ src/main/java/com/jcba/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.jcba.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.jcba.cursomc.domain.Pedido;
@@ -7,6 +9,10 @@ import com.jcba.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
